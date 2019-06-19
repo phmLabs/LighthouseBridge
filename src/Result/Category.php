cf1796c5abb5eb05e58f66482b86cce3b3b6e8db
Nils Langner <nils.langner@leankoala.com>
2019-06-19 17:15:43 +0200

    undefined score handling

--- src/Result/Category.php
+++ src/Result/Category.php
@@ -4,6 +4,8 @@ namespace phmLabs\LighthouseBridge\Result;
 
 class Category
 {
+    const SCORE_UNDEFINED = 'undefined';
+
     private $categoryArray;
 
     public function __construct(array $categoryArray)
@@ -13,6 +15,10 @@ class Category
 
     public function getScore()
     {
-        return $this->categoryArray['score'] * 100;
+        if ($this->categoryArray['score'] == 'null') {
+            return self::SCORE_UNDEFINED;
+        } else {
+            return $this->categoryArray['score'] * 100;
+        }
     }
 }
